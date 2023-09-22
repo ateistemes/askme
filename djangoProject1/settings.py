188ffd287f27c0398c180a87cdcdd47f3b7751cc
ateistemes <timur.tynychbekov@gmail.com>
2023-09-22 20:20:16 +0600

    best practice fixed INSTALLED_APPS

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'askme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'askme.apps.AskmeConfig', #my app askme
 ]
 
 MIDDLEWARE = [
